f94762239a527d7ffe01970b62c67cd6434cd34e
Zuitow <pc.zuit@gmail.com>
2024-07-31 09:43:23 -0300

    commit

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import { styles } from "../styles/HomeStyles";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to BeGlass</Text>
+    </View>
+  );
+}
